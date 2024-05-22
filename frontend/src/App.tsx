57d0b3bc6a7abfe79a8b9e9ad5213718304cec42
rserdah <94141015+rserdah@users.noreply.github.com>
2024-05-22 16:15:16 -0400

    Made the form appear on the front page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 import { Container, Row, Form, FloatingLabel, Button } from 'react-bootstrap';
 import PatientForm from './components/OtherForms';
 import NewPatientForm from './components/forms/NewPatientForm';
+import AdmissionForm from './components/forms/AdmissionForm';
 
 const App = () => {
   return (
     <>
-      <NewPatientForm></NewPatientForm>
+      <AdmissionForm />
     </>
   );
 };
